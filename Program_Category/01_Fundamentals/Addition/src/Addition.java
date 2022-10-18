d98e29223077e18cfff2daccf2490aec1365b36a
Aishwarya Patil <92450677+b-aishpatil@users.noreply.github.com>
2022-10-18 19:30:34 +0530

    Create Addition.java

---
+++ Program_Category/01_Fundamentals/Addition/src/Addition.java
@@ -0,0 +1,22 @@
+// Java Program to Add Two Numbers
+import java.util.Scanner;
+public class AddTwoNumbers{
+    public static void main (String[] args){
+    
+    // Variables
+    int num1, num2, sum;
+    
+    // Asking for input
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter first number: ");
+    num1 = sc.nextInt();
+    System.out.println("Enter second number: ");
+    num2 = sc.nextInt();
+    
+    // Addition
+    sum = num1 + num2;
+    
+    // Displaying output
+    System.out.println("Sum of two numbers: " + sum);
+    }
+}
