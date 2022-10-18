d8f456bd7a0f3874f9b1b04646f60eeb2664a833
RhinoCoder01 <116112007+RhinoCoder01@users.noreply.github.com>
2022-10-19 00:54:06 +0530

    added cpp code for linear search through recursion

---
+++ Program_Category/03 Algorithm/Searching Algorithms/Linear Search/linear_search_recursive.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+// Recursive function to search for x in arr[]
+int searchElement(int arr[], int size, int x) {
+	
+	size--;
+	
+	// Base case (Element not present in the array)
+	if (size < 0) {
+		return -1;
+	}
+	// Base case (Element found, return its position)
+	if (arr[size] == x) {
+		return size;
+	}
+
+	// Recursive case
+	return searchElement(arr, size, x);
+
+}
+
+
+// Driver code
+int main() {
+	int arr[] = {17, 15, 11, 8, 13, 19};
+	int size = sizeof(arr) / sizeof(arr[0]);
+	int x = 11;
+	int idx = searchElement(arr, size, x);
+	if (idx != -1)
+		cout << "Element " << x << " is present at index " <<idx;
+	else
+		cout << "Element " << x << " is not present in the array";
+	return 0;
+}
